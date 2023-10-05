b86307fa56b19b01999eae32f776dcf7d0196a91
abdellah585 <abdellah585@gmail.com>
2023-10-05 14:56:43 -0700

    Fix Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
+ * main - Prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
