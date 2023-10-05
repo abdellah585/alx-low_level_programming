2fc5d492d3632bf3f6a12a6fadc82f6a91d45b70
abdellah585 <abdellah585@gmail.com>
2023-10-05 15:28:27 -0700

    Fix task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
@@ -6,6 +7,6 @@
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59)
+	write(1,quo,59);
 	return (1);
 }
