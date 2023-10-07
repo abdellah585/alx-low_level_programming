272bd853fa56b70b0ecfe530b83c13c033b6d538
abdellah585 <abdellah585@gmail.com>
2023-10-06 19:54:58 -0700

    Add task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ * 		combination of two digits.
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	a=0;
+
+	while (a<100)
+	{
+		b = a % 10;
+		c = a / 10;
+
+		if(c<b)
+		{
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a<89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
