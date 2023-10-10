f8e27a82536cbe04aa29dafc1520c0e7b8a5843e
abdellah585 <abdellah585@gmail.com>
2023-10-10 11:01:29 -0700

    Fix task1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - print _putchar followed by a new line
+ * main - prints _putchar followed by a new line
  * return : Always 0 (Success)
  */
 int void(main)
@@ -18,13 +18,3 @@ int void(main)
 
 	return(0);
 }
-
-
-
-
-
-
-
-
-
-
