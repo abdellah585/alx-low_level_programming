252275a0d799fceff8779c7dab1ebb01e928cd38
abdellah585 <abdellah585@gmail.com>
2023-10-16 07:31:44 -0700

    Fix task 2 Add task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
+ * _puts - prints a string, followed by a new line, to stdout
  *
+ * @s : pointer to a string
+ *
+ * Return : void
+ */
+
+void _puts(char *s)
+{
+	int i = 0;
+	while( *(s+i) != '/0' )
+	{
+		putchar(*(s+i));
+		i++;
+	}
+}
