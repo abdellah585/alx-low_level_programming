916ecbda859d5bf1de5f3c3233a45a76bed2dcb7
abdellah585 <abdellah585@gmail.com>
2023-10-25 02:06:58 -0700

    Add task 8 folder

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string to evaluate
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+
