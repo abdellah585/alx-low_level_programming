c41163b94d9680a4932335f452396ccc2a980501
abdellah585 <abdellah585@gmail.com>
2023-10-27 12:17:12 -0700

    Add project 0x09

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @s: string to search
+ * @c: character to find
+ *
+ * Return: returns a pointer to the first occurence of the
+ * character c in the string s or NULL if char not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (1)
+	{
+		i = *s++;
+		if (i == c)
+		{
+			return (s - 1);
+		}
+		if (i == 0)
+		{
+			return (NULL);
+		}
+	}
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string
+ *
+ * @s: pointer to a string
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
