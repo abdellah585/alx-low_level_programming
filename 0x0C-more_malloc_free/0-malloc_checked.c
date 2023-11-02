21c932b9455a0027728082f9c6d75c474e843b83
abdellah585 <abdellah585@gmail.com>
2023-11-02 06:17:37 -0700

    Fix task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,8 @@
  * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return: a pointer to the allocated memory else exit code 98
+ * Return: a pointer to the allocated memory
+ * else exit code 98
  */
 void *malloc_checked(unsigned int b)
 {
